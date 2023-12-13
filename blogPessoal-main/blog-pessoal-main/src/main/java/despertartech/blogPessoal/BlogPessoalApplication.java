b12b7bea2bac6af80405f820b09e9dbd448d3621
wendyy <132934801+WENDELLYAN15@users.noreply.github.com>
2023-12-13 15:36:30 -0300

    Update BlogPessoalApplication.java

--- blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoal/BlogPessoalApplication.java
+++ blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoal/BlogPessoalApplication.java
@@ -1,4 +1,4 @@
-package despertartech.blogPessoal;
+package despertartech.blogAquiTem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
