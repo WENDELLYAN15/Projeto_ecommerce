568e2f169d43229480848ffe57c1764a2849559f
wendyy <132934801+WENDELLYAN15@users.noreply.github.com>
2023-12-13 15:39:23 -0300

    Update produtoRepository.java

--- blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoal/repository/produtoRepository.java
+++ blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoal/repository/produtoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
-import despertartech.blogPessoal.model.produto;
+import despertartech.blogAquiTem.model.produto;
 
 @Repository
 public interface produtoRepository extends JpaRepository<produto, Long> {	
