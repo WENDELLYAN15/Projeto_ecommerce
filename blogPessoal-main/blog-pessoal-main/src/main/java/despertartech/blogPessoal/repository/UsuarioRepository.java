5027e7ef831635a3318ffeef23083f1c89e69f8d
wendyy <132934801+WENDELLYAN15@users.noreply.github.com>
2023-12-13 15:38:38 -0300

    Update UsuarioRepository.java

--- blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoal/repository/UsuarioRepository.java
+++ blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoal/repository/UsuarioRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import despertartech.blogPessoal.model.Usuario;
+import despertartech.blogAquiTem.model.Usuario;
 
 
 
