206af7467ccca5f54216e048b353a2d8d3523bc6
wendyy <132934801+WENDELLYAN15@users.noreply.github.com>
2023-12-13 15:38:59 -0300

    Update categoriaRepository.java

--- blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoal/repository/categoriaRepository.java
+++ blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoal/repository/categoriaRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import despertartech.blogPessoal.model.categoria;
+import despertartech.blogAquiTem.model.categoria;
 
 public interface categoriaRepository extends JpaRepository<categoria, Long> {
 	public List<categoria> findAllByDescricaoContainingIgnoreCase( String descricao);
