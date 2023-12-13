a3d6f8471ea01d224c9aa1c07abbd11c5f1bc293
wendyy <132934801+WENDELLYAN15@users.noreply.github.com>
2023-12-13 15:47:26 -0300

    Update BasicSecurityConfig.java

--- blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoalseguranca/BasicSecurityConfig.java
+++ blogPessoal-main/blog-pessoal-main/src/main/java/despertartech/blogPessoalseguranca/BasicSecurityConfig.java
@@ -1,4 +1,4 @@
-package despertartech.blogPessoalseguranca;
+package despertartech.blogAquiTemseguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
